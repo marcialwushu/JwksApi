e4a6f15be2d387e3c3e947fe92fdcd5c7c4f1099
cleilson pereira <marcialwushu@hotmail.com>
2024-06-08 22:04:25 -0300

    Adicionar arquivos de projeto.

---
+++ JwksApi/Controllers/JwkController.cs
@@ -0,0 +1,46 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using Newtonsoft.Json;
+using System;
+using System.Security.Cryptography;
+using System.Security.Cryptography.X509Certificates;
+using JwksApi.Models;
+using System.Text.RegularExpressions;
+
+namespace JwksApi.Controllers
+{
+    [ApiController]
+    [Route("api/[controller]")]
+    public class JwkController : ControllerBase
+    {
+        private JwkKey GenerateJwkFromPfx(string pfxPath, string password)
+        {
+            using var certificate = new X509Certificate2(pfxPath, password, X509KeyStorageFlags.Exportable);
+            using var rsa = certificate.GetRSAPublicKey();
+            var parameters = rsa.ExportParameters(false);
+            var modulus = Convert.ToBase64String(parameters.Modulus);
+            var exponent = Convert.ToBase64String(parameters.Exponent);
+
+            var x5c = new[] { Convert.ToBase64String(certificate.RawData) };
+            var thumbprint = Convert.ToBase64String(certificate.GetCertHash());
+
+            return new JwkKey
+            {
+                KeyType = "RSA",
+                Use = "sig",
+                KeyId = certificate.Thumbprint,
+                Modulus = modulus,
+                Exponent = exponent,
+                X5c = x5c,
+                X5t = thumbprint
+            };
+        }
+
+        [HttpGet("jwks.json")]
+        public IActionResult GetJwk()
+        {
+            var jwk = GenerateJwkFromPfx("C:/Users/marci/source/repos/service/AuthSampleCode/GenerateJwt/jwt.sample.code.pfx", "123456");
+            var json = JsonConvert.SerializeObject(jwk);
+            return Ok(json);
+        }
+    }
+}
---
+++ JwksApi/Models/Jwk.cs
@@ -0,0 +1,28 @@
+﻿using Newtonsoft.Json;
+
+namespace JwksApi.Models
+{
+    public class JwkKey
+    {
+        [JsonProperty("kty")]
+        public string KeyType { get; set; }
+
+        [JsonProperty("use")]
+        public string Use { get; set; }
+
+        [JsonProperty("kid")]
+        public string KeyId { get; set; }
+
+        [JsonProperty("n")]
+        public string Modulus { get; set; }
+
+        [JsonProperty("e")]
+        public string Exponent { get; set; }
+
+        [JsonProperty("x5c")]
+        public string[] X5c { get; set; }
+
+        [JsonProperty("x5t")]
+        public string X5t { get; set; }
+    }
+}
